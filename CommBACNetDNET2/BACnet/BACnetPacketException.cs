764094719456eeac13f43d71d95b26e275770bfb
yoo <yoo@DESKTOP-D9MT2Q6>
2019-03-25 17:39:41 +0900

    dd

---
+++ CommBACNetDNET2/BACnet/BACnetPacketException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WooriSI.BACnet
+{
+    [Serializable]
+    internal class BACnetPacketException : Exception
+    {
+        public BACnetPacketException(string message)
+            : base(message)
+        {
+        }
+    }
+}
